91a64d8a6b640634e633d1ddc31826cac5417521
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2015-04-30 12:12:23 -0400

    create working tabs

--- dogPlugin/tabs.js
+++ dogPlugin/tabs.js
@@ -1,11 +1,26 @@
 $.Tabs = function (el) {
   this.$el = $(el);
-  this.$links = $($el.find("tabs"));
-  this.$contentTabs = $($links.find("#"+data["content-tabs"]));
+  this.$contentTabs = $("#"+this.$el.data("content-tabs"));
   this.$tabPanes = $(this.$contentTabs.find("tab-pane"));
-  this.$activeTab = $($contentTabs.find(".active-pane"));
-
+  this.$activeTab = $(this.$contentTabs.find(".active-pane"));
+  this.$el.on("click","a", this.clickTab.bind(this));
 };
+
+$.Tabs.prototype.clickTab = function(event) {
+  event.preventDefault();
+  this.$activeTab.removeClass("active-pane");
+  var $activeLink = $(event.currentTarget);
+  $activeLink.addClass("active");
+  var tabName = $activeLink.attr('href');
+  this.$activeTab = $(this.$contentTabs.find(tabName));
+  this.$activeTab.addClass("active-pane");
+};
+
+
+
+
+
+
 
 $.fn.tabs = function () {
   return this.each(function () {
