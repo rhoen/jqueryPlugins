4b0b0a58abeeacba35648d5820c7d17aa2c2d669
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2015-04-30 12:22:57 -0400

    Make everything awesome

--- dogPlugin/tabs.js
+++ dogPlugin/tabs.js
@@ -3,15 +3,19 @@ $.Tabs = function (el) {
   this.$contentTabs = $("#"+this.$el.data("content-tabs"));
   this.$tabPanes = $(this.$contentTabs.find("tab-pane"));
   this.$activeTab = $(this.$contentTabs.find(".active-pane"));
+  this.$activeLink = $(this.$el.find(".active-link"));
   this.$el.on("click","a", this.clickTab.bind(this));
 };
 
 $.Tabs.prototype.clickTab = function(event) {
   event.preventDefault();
   this.$activeTab.removeClass("active-pane");
-  var $activeLink = $(event.currentTarget);
-  $activeLink.addClass("active");
-  var tabName = $activeLink.attr('href');
+  this.$activeLink.removeClass("active-link");
+
+  this.$activeLink = $(event.currentTarget);
+  this.$activeLink.addClass("active-link");
+
+  var tabName = this.$activeLink.attr('href');
   this.$activeTab = $(this.$contentTabs.find(tabName));
   this.$activeTab.addClass("active-pane");
 };
