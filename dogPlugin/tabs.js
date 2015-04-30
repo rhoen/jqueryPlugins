e81602d244b9e98f388a4be88eecf51e21e33860
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2015-04-30 11:47:14 -0400

    create constructor for Tabs

--- dogPlugin/tabs.js
+++ dogPlugin/tabs.js
@@ -1,7 +1,9 @@
 $.Tabs = function (el) {
-
-
-
+  this.$el = $(el);
+  this.$links = $($el.find("tabs"));
+  this.$contentTabs = $($links.find("#"+data["content-tabs"]));
+  this.$tabPanes = $(this.$contentTabs.find("tab-pane"));
+  this.$activeTab = $($contentTabs.find(".active-pane"));
 
 };
 
