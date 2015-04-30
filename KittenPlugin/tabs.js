13b483117db5c004642076677f77dce0d3b2960e
App Academy Student <appacademy@Apps-Mac-mini-7.local>
2015-04-30 15:32:41 -0400

    Add ability to swap through pictures

--- KittenPlugin/tabs.js
+++ KittenPlugin/tabs.js
@@ -1,1 +1,22 @@
-$.Carousel =
+$.Carousel = function(el) {
+  this.$el = $(el);
+  this.$items = this.$el.find("items");
+  this.activeIndex = 0;
+}
+
+$.Carousel.prototype.slide(dir) {
+  if (this.activeIndex + dir < 0) {
+    return;
+  } else if (this.activeIndex + dir > this.$items.find("li").length) {
+    return;
+  }
+
+  var newIndex = this.activeIndex + dir;
+  var newChild = this.$items.children().eq(newIndex);
+  var oldChild = this.$items.children().eq(this.activeIndex);
+
+  oldChild.removeClass("active");
+  newChild.addClass("active");
+
+
+};
